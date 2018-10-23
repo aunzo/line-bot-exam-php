64a3d7ebe45ff74797d4b4bd4a6b196bd9860671
Patarapon Tohkam <patarapon.t@gmail.com>
2018-10-23 20:22:12 +0700

    add line sdk

---
+++ bot_settings.php
@@ -0,0 +1,7 @@
+<?php
+/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
+define('LINE_MESSAGE_CHANNEL_ID','1616701754');
+define('LINE_MESSAGE_CHANNEL_SECRET','b3381416298649a5ab506abe5dcfd1f1');
+define('LINE_MESSAGE_ACCESS_TOKEN','
+m6q4BuO+XPAbXXrJ8DuzWTf65E/+pdJqp01WtMDx1Wytd1vgaFikrhRpbm8cijTGdex9MN5ic0GycS/T6cdlpcBs9FbXqRRu04imZtSw+/TabCziLtLnhe8O6KMNFVhAQvECMAAzonhwCTyPC19riwdB04t89/1O/w1cDnyilFU=');
+?>
